d14a67c1ff2a4e1b4aa4f28f8d3bfba78184228a
Zino <41270843+zungf@users.noreply.github.com>
2018-11-21 14:04:46 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const moment = require('moment');
 const jimp = require('jimp');
 const Canvas = require('canvas');
+
  
 client.on('guildMemberAdd', member => {
      const welcomer =  member.guild.channels.find('name', 'txt');
